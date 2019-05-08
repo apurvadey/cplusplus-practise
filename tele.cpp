0c00912a4f3851ea712871d15323000c4f3d2bc4
Apurva Dey <apurvadey51@gmail.com>
2019-05-09 01:19:26 +0530

    Cleanup code using cpplint

--- tele.cpp
+++ tele.cpp
@@ -3,45 +3,50 @@
 #include<cstring>
 using namespace std;
 
-class telephone{
-    long no;
-    char first[10];
-    char last[10];
-    char mail[20];
-    static int count;
-    public:
-       void set_data(){
-           cout<<"Enter the first name";
-           cin>>first;
-           cout<<endl<<"Enter the last name";
-           cin>>last;
-           cout<<"Enter the phone number";
-           cin>>no;
-           cout<<endl<<"Enter the email id";
+class telephone {
+  long no;
+  char first[10];
+  char last[10];
+  // That limit is a maximum of 64 characters 
+  //(octets) in the 
+  //“local part” (before the “@”) and a maximum of 255 characters (octets) in the domain part (after the “@”) for a total length of 320 characters. However, there is a restriction in RFC 2821 on the length of an address in MAIL and RCPT commands of 254 characters.
+  char mail[320];
+  static int count;
 
-           cin>>mail;
+  public:
+    void set_data() {
+      cout << "Enter the first name";
+      cin >> first;
+      cout << endl << "Enter the last name";
+      cin >> last;
+      cout << "Enter the phone number";
+      cin >> no;
+      cout << endl << "Enter the email id";
 
-           count++;
-       }
+      cin >> mail;
 
-       void display(){
-           cout<<endl<<count<<". "<<first<<" "<<last<<" "<<no<<" "<<mail;
-       }
+      count++;
+    }
+
+    void display() {
+      cout << endl << count << ". " << first << " " << last << " " << no << " " << mail;
+    }
 
 };
 
-int telephone::count=0;
+int telephone::count = 0;
 
-main()
-{
+int main() {
     telephone t[20];
     int i;
     int n;
-    cout<<"Enter the no. of contacts";
-    cin>>n;
-    for(i=0;i<n;i++)
+    cout << "Enter the no. of contacts";
+    cin >> n;
+    for (i = 0; i < n; i++) {
         t[i].set_data();
-    for(i=0;i<n;i++)
+    }
+    for (i = 0; i < n; i++) {
         t[i].display();
+    }
     return 0;
 }
